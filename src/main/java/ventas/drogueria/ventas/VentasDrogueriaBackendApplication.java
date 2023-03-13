cdbdc66c21cf7fca3337e090abd45d54519edc80
Alejandro Couoh <alejandro.couoh@gmail.com>
2023-03-13 11:05:53 -0600

    Archivos agregados

---
+++ src/main/java/ventas/drogueria/ventas/VentasDrogueriaBackendApplication.java
@@ -0,0 +1,13 @@
+package ventas.drogueria.ventas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VentasDrogueriaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VentasDrogueriaBackendApplication.class, args);
+	}
+
+}
